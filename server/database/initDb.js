4afe191566e56d0c283c15345c07163cb0b7577c
Joseph Fiore <98jfiore@gmail.com>
2021-04-20 19:45:31 -0400

    Revert "Get database fully working on .env values"
    This reverts commit f02366974e512e886f895d07306a0233bc69886c.

--- server/database/initDb.js
+++ server/database/initDb.js
@@ -1,12 +1,9 @@
-require('dotenv').config();
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-	host: process.env.MYSQLHOST,
-	port: process.env.MYSQLPORT,
-	user: process.env.MYSQLUSER,
-	password: process.env.MYSQLPASS,
-	insecureAuth: true
+	host: 'localhost',
+	user: 'root',
+	password: '123',
 });
 
 con.connect((err) => {
