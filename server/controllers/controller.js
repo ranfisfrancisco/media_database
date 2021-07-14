d2deed62cdc8241d197cf764c683b4c42c786b94
Ranfis Francisco <ranfis.francisco@gmail.com>
2021-07-13 20:22:21 -0400

    able to make sql query

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -5,13 +5,9 @@ module.exports.home = async (req, res) => {
 }
 
 module.exports.getAllMedia = async (req, res) => {
-	//let { bId, docId, copyNo, position } = req.body;
-	//position = position ? position : 'NULL';
-	console.log("GetAllMedia")
-	let query = `SELECT * FROM MEDIA`; 
+	let query = `SELECT * FROM media_items`; 
 	conn.query(query, (err, result) => {
 		if(err) return res.status(400).json({ message: 'Query error' });
 		res.send({ result });
 	});
 }
-
