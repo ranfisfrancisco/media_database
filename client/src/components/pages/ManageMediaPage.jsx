0694cadb193f29e799210d3a9d7c7085c24f4b2f
Ranfis Francisco <ranfis.francisco@gmail.com>
2021-08-07 13:55:44 -0400

    added row counter to button

--- client/src/components/pages/ManageMediaPage.jsx
+++ client/src/components/pages/ManageMediaPage.jsx
@@ -437,7 +437,7 @@ const ManageMediaPage = () => {
 
 		return (
 		<div>
-			<Button onClick={deselectRows}>Deselect All</Button>
+			<Button onClick={deselectRows}>Deselect {selectedRows.length}</Button>
 			<Table dataSource={dataSource} columns={columns} rowSelection={rowSelection} rowKey={record =>record.media_id} />
 		</div>
 		);
